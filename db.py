c753478565c885a54eee33d21f1f3db88f734688
Gregg Midon <3g.sr.yo@gmail.com>
2017-11-29 10:30:42 -0500

    Update db.py

--- db.py
+++ db.py
@@ -9,6 +9,6 @@ import cx_Oracle
 
 def connect_to_db():
     #note: function to retrieve connection string
-    #return cx_Oracle.connect('demki/demd1892@demd')
-    return cx_Oracle.connect('demki/dem18ki92@demp')
-    #return cx_Oracle.connect('demki/dem18ki92@demq')
+    #return cx_Oracle.connect('*****/******@****')
+    return cx_Oracle.connect('*****/******@****')
+    #return cx_Oracle.connect('*****/*******@****')
